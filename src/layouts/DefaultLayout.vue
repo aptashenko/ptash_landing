40f54575031a8f457e08846749418e5baea1cd95
Artem Ptashenko <aptashenko2019@gmail.com>
2023-10-22 02:28:17 +0300

    add dist

--- src/layouts/DefaultLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <the-header />
-  <slot />
-  <the-footer />
-</template>
-<script setup>
-import TheHeader from "@/components/TheHeader.vue";
-import TheFooter from "@/components/TheFooter.vue";
-</script>
